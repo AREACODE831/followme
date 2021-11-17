100744f10169166cc184eafc9e5e9dbc8a01d922
Soovin Choi <soovinnnnnn@gmail.com>
2021-11-17 03:59:51 -0800

    s

--- client7.js
+++ client7.js
@@ -3,11 +3,11 @@
 // A three.js scene which uses planes and texture loading to generate a scene with images which can be traversed with basic WASD and mouse controls, this scene is full screen with an overlay.
 
 // Import required source code
-
+// Import three.js core
 import * as THREE from "./build/three.module.js";
-import {PointerLockControls} from "./src/PointerLockControls.js";
+// Import pointer lock controls
+import {  PointerLockControls} from "./src/PointerLockControls.js";
 
-import Stats from "./src/stats.module.js";
 
 import { FirstPersonControls } from "./src/FirstPersonControls.js";
 import { GLTFLoader } from "./src/GLTFLoader.js";
@@ -15,278 +15,111 @@ import { FontLoader } from "./src/FontLoader.js";
 import { TextGeometry } from "./src/TextGeometry.js";
 
 
+import { GUI } from './src/dat.gui.module.js';
 
-// Establish variables
-let camera, scene, renderer, controls, material;
+import { MapControls } from './src/OrbitControls.js';
 
-const objects = [];
-let raycaster;
+let camera, controls, scene, renderer;
 
-let moveForward = false;
-let moveBackward = false;
-let moveLeft = false;
-let moveRight = false;
-let canJump = false;
-
-let prevTime = performance.now();
-const velocity = new THREE.Vector3();
-const direction = new THREE.Vector3();
-const vertex = new THREE.Vector3();
-const color = new THREE.Color();
-
-// Initialization and animation function calls
 init();
+//render(); // remove when using next line for animation loop (requestAnimationFrame)
 animate();
 
-// Initialize the scene
 function init() {
-  // Establish the camera
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    1,
-    1000
-  );
-  camera.position.y = 10;
 
-  // Define basic scene parameters
   scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x000000);
-  scene.fog = new THREE.Fog(0xffffff, 0, 750);
+  scene.background = new THREE.Color( 0xcccccc );
+  scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
 
-  // Define scene lighting
-  const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
-  light.position.set(0.5, 1, 0.75);
-  scene.add(light);
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
 
-  // Define controls
-  controls = new PointerLockControls(camera, document.body);
+  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera.position.set( 400, 200, 0 );
 
-  // Identify the html divs for the overlays
-  const blocker = document.getElementById("blocker");
-  const instructions = document.getElementById("instructions");
+  // controls
 
-  // Listen for clicks and respond by removing overlays and starting mouse look controls
-  // Listen
-  instructions.addEventListener("click", function() {
-    controls.lock();
-  });
-  // Remove overlays and begin controls on click
-  controls.addEventListener("lock", function() {
-    instructions.style.display = "none";
-    blocker.style.display = "none";
-  });
-  // Restore overlays and stop controls on esc
-  controls.addEventListener("unlock", function() {
-    blocker.style.display = "block";
-    instructions.style.display = "";
-  });
-  // Add controls to scene
-  scene.add(controls.getObject());
+  controls = new MapControls( camera, renderer.domElement );
 
-  // Define key controls for WASD controls
-  const onKeyDown = function(event) {
-    switch (event.code) {
-      case "ArrowUp":
-      case "KeyW":
-        moveForward = true;
-        break;
+  //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
 
-      case "ArrowLeft":
-      case "KeyA":
-        moveLeft = true;
-        break;
+  controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+  controls.dampingFactor = 0.05;
 
-      case "ArrowDown":
-      case "KeyS":
-        moveBackward = true;
-        break;
+  controls.screenSpacePanning = false;
 
-      case "ArrowRight":
-      case "KeyD":
-        moveRight = true;
-        break;
+  controls.minDistance = 100;
+  controls.maxDistance = 500;
 
-      case "Space":
-        if (canJump === true) velocity.y += 350;
-        canJump = false;
-        break;
-    }
-  };
+  controls.maxPolarAngle = Math.PI / 2;
 
-  const onKeyUp = function(event) {
-    switch (event.code) {
-      case "ArrowUp":
-      case "KeyW":
-        moveForward = false;
-        break;
+  // world
 
-      case "ArrowLeft":
-      case "KeyA":
-        moveLeft = false;
-        break;
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  geometry.translate( 0, 0.5, 0 );
+  const material = new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } );
 
-      case "ArrowDown":
-      case "KeyS":
-        moveBackward = false;
-        break;
+  for ( let i = 0; i < 500; i ++ ) {
 
-      case "ArrowRight":
-      case "KeyD":
-        moveRight = false;
-        break;
-    }
-  };
+    const mesh = new THREE.Mesh( geometry, material );
+    mesh.position.x = Math.random() * 1600 - 800;
+    mesh.position.y = 0;
+    mesh.position.z = Math.random() * 1600 - 800;
+    mesh.scale.x = 20;
+    mesh.scale.y = Math.random() * 80 + 10;
+    mesh.scale.z = 20;
+    mesh.updateMatrix();
+    mesh.matrixAutoUpdate = false;
+    scene.add( mesh );
 
-  document.addEventListener("keydown", onKeyDown);
-  document.addEventListener("keyup", onKeyUp);
-
-  // Add raycasting for mouse controls
-  raycaster = new THREE.Raycaster(
-    new THREE.Vector3(),
-    new THREE.Vector3(0, -1, 0),
-    0,
-    10
-  );
-
-  // Generate the ground
-  let floorGeometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
-  floorGeometry.rotateX(-Math.PI / 2);
-
-  // Vertex displacement pattern for ground
-  let position = floorGeometry.attributes.position;
-
-  for (let i = 0, l = position.count; i < l; i++) {
-    vertex.fromBufferAttribute(position, i);
-
-    vertex.x += Math.random() * 20 - 10;
-    vertex.y += Math.random() * 2;
-    vertex.z += Math.random() * 20 - 10;
-
-    position.setXYZ(i, vertex.x, vertex.y, vertex.z);
   }
 
-  floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+  // lights
 
-  position = floorGeometry.attributes.position;
-  const colorsFloor = [];
+  const dirLight1 = new THREE.DirectionalLight( 0xffffff );
+  dirLight1.position.set( 1, 1, 1 );
+  scene.add( dirLight1 );
 
-  for (let i = 0, l = position.count; i < l; i++) {
-    color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-    colorsFloor.push(color.r, color.g, color.b);
-  }
+  const dirLight2 = new THREE.DirectionalLight( 0x002288 );
+  dirLight2.position.set( - 1, - 1, - 1 );
+  scene.add( dirLight2 );
 
-  floorGeometry.setAttribute(
-    "color",
-    new THREE.Float32BufferAttribute(colorsFloor, 3)
-  );
+  const ambientLight = new THREE.AmbientLight( 0x222222 );
+  scene.add( ambientLight );
 
-  const floorMaterial = new THREE.MeshBasicMaterial({
-    vertexColors: true
-  });
+  //
 
-  const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-
-  // Insert completed floor into the scene
-  scene.add(floor);
+  window.addEventListener( 'resize', onWindowResize );
 
 
-  // First Image (red and purple glitch map)
-  // Load image as texture
-  const texture = new THREE.TextureLoader().load( './assets/glitch_map.jpg' );
-  // Immediately use the texture for material creation
-  const material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide } );
-  // Create plane geometry
-  const geometry = new THREE.PlaneGeometry( 32, 16 );
-  // Apply image texture to plane geometry
-  const plane = new THREE.Mesh( geometry, material );
-  // Position plane geometry
-  plane.position.set(0 , 15 , -15);
-  // Place plane geometry
-  scene.add( plane );
+  const gui = new GUI();
+  gui.add( controls, 'screenSpacePanning' );
 
-  // Second Image (Text with image and white background)
-  // Load image as texture
-  const texture2 = new THREE.TextureLoader().load( './assets/bouy.jpg' );
-  // immediately use the texture for material creation
-  const material2 = new THREE.MeshBasicMaterial( { map: texture2, side: THREE.DoubleSide } );
-  // Create plane geometry
-  const geometry2 = new THREE.PlaneGeometry( 200, 100 );
-  // Apply image texture to plane geometry
-  const plane2 = new THREE.Mesh( geometry2, material2 );
-  // Position plane geometry
-  plane2.position.set(0 , 100 , -200);
-  // Place plane geometry
-  scene.add( plane2 );
-
-  // Define Rendered and html document placement
-  renderer = new THREE.WebGLRenderer({
-    antialias: true
-  });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
-  // Listen for window resizing
-  window.addEventListener("resize", onWindowResize);
 }
 
-// Window resizing function
 function onWindowResize() {
+
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
 }
 
-// Animation function
 function animate() {
-  requestAnimationFrame(animate);
 
-  const time = performance.now();
+  requestAnimationFrame( animate );
 
-  // Check for controls being activated (locked) and animate scene according to controls
-  if (controls.isLocked === true) {
-    raycaster.ray.origin.copy(controls.getObject().position);
-    raycaster.ray.origin.y -= 10;
+  controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
 
-    const intersections = raycaster.intersectObjects(objects, false);
+  render();
 
-    const onObject = intersections.length > 0;
+}
 
-    const delta = (time - prevTime) / 1000;
+function render() {
 
-    velocity.x -= velocity.x * 10.0 * delta;
-    velocity.z -= velocity.z * 10.0 * delta;
+  renderer.render( scene, camera );
 
-    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-
-    direction.z = Number(moveForward) - Number(moveBackward);
-    direction.x = Number(moveRight) - Number(moveLeft);
-    direction.normalize(); // this ensures consistent movements in all directions
-
-    if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
-    if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
-
-    if (onObject === true) {
-      velocity.y = Math.max(0, velocity.y);
-      canJump = true;
-    }
-
-    controls.moveRight(-velocity.x * delta);
-    controls.moveForward(-velocity.z * delta);
-
-    controls.getObject().position.y += velocity.y * delta; // new behavior
-
-    if (controls.getObject().position.y < 10) {
-      velocity.y = 0;
-      controls.getObject().position.y = 10;
-
-      canJump = true;
-    }
-  }
-
-  prevTime = time;
-
-  renderer.render(scene, camera);
 }
